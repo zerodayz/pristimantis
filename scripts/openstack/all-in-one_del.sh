40fafefd5c33c18d17c17252cb22de5a29f38baf
zerodayz <cerninr@gmail.com>
2018-08-29 15:34:59 +1000

    Adding removal script

--- scripts/openstack/all-in-one_del.sh
+++ scripts/openstack/all-in-one_del.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 sh delete-flavor.sh
-sh delete-image.sh
-sh delete-network.sh
-sh delete-subnet.sh
-sh delete-router.sh
 sh delete-instance.sh
+sh delete-router.sh
+sh delete-subnet.sh
+sh delete-network.sh
+sh delete-image.sh
