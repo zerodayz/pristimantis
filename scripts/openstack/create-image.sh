4578cff106a0442f8ce20baba5e1c9d1487b8abf
zerodayz <cerninr@gmail.com>
2018-08-29 14:56:47 +1000

    Add download link for cirros

--- scripts/openstack/create-image.sh
+++ scripts/openstack/create-image.sh
@@ -14,5 +14,6 @@ source $HOME/overcloudrc
 image=$( glance image-list | grep "\s$ITEM_NAME[^.]" | awk '{ print $2 }')
 
 if [ -z $image ]; then
+  wget "http://download.cirros-cloud.net/0.3.3/cirros-0.3.3-x86_64-disk.img" -O "$HOME/cirros-0.3.3-x86_64-disk.img"
   glance image-create --name $ITEM_NAME --disk-format qcow2 --container-format bare --file $ITEM_LOCATION
 fi
