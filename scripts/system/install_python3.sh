83871d81b32d88b04852f979429b001de939ddeb
Robin Cernin <cerninr@gmail.com>
2019-04-17 13:48:29 +1000

    Adding install_python3 script

---
+++ scripts/system/install_python3.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd /tmp
+echo "Downloading python 3"
+wget https://www.python.org/ftp/python/3.7.3/Python-3.7.3.tar.xz
+echo "Extracting python3"
+tar -xf Python-3.7.3.tar.xz
+cd Python-3.7.3
+echo "Configuring python3"
+./configure --enable-optimizations
+echo "Installing python3"
+make altinstall                     
+echo "Making symlink"
+ln -s /usr/bin/python3.7 /usr/local/bin/python3
